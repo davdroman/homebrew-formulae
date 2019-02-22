9241c259cc6cf887a8c8f911ed4da132a35a55fe
DavdRoman <d@vidroman.me>
2019-02-22 14:55:02 +0000

    Fix test step

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -1,11 +1,8 @@
 class Badonde < Formula
   desc "Effortless PR creation tool"
   homepage "https://github.com/DavdRoman/Badonde"
-  url "https://github.com/DavdRoman/Badonde.git",
-      :tag => "1.3.0",
-      :shallow => false
-  head "https://github.com/DavdRoman/Badonde.git",
-      :shallow => false
+  url "https://github.com/DavdRoman/Badonde.git", :tag => "1.3.0"
+  head "https://github.com/DavdRoman/Badonde.git"
 
   depends_on :xcode => ["10.1", :build]
 
@@ -14,6 +11,6 @@ class Badonde < Formula
   end
 
   test do
-    system "#{bin}/badonde" "version"
+    system "#{bin}/badonde"
   end
 end
