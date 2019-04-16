b0c09ff174d42f40abdccb79878e00ce06f7f779
davdroman <d@vidroman.dev>
2019-04-16 14:14:07 +0100

    Add revision commit

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -1,7 +1,8 @@
 class Badonde < Formula
   desc "Painless PR-ing"
   homepage "https://github.com/DavdRoman/Badonde"
-  url "https://github.com/DavdRoman/Badonde.git", :tag => "1.8.1"
+  url "https://github.com/DavdRoman/Badonde.git",
+    :tag => "1.8.1", :revision => "c551fa3f3259a5475c4d90a7fd126709202ef16d"
   head "https://github.com/DavdRoman/Badonde.git"
 
   depends_on :xcode => ["10.1", :build]
