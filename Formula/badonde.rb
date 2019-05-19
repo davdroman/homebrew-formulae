c3bae044db4cf424950d29041cc3182fd6f961db
davdroman <d@vidroman.dev>
2019-05-19 03:17:21 +0100

    Bump version number (1.12.1)

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -2,7 +2,7 @@ class Badonde < Formula
   desc "Painless PR-ing"
   homepage "https://github.com/DavdRoman/Badonde"
   url "https://github.com/DavdRoman/Badonde.git",
-    :tag => "1.12.0", :revision => "0d442811ec9c31851794afa66f9649f092290d0a"
+    :tag => "1.12.1", :revision => "3c44fe10fa943ffdc80d5c1786cc357fe9a7fb93"
   head "https://github.com/DavdRoman/Badonde.git"
 
   depends_on :xcode => ["10.2", :build]
