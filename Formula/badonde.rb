f5038b9b5b60be74047be08453d9f9a575d7ac8f
davdroman <d@vidroman.dev>
2019-06-18 12:44:57 +0100

    Gardening

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -2,7 +2,7 @@ class Badonde < Formula
   desc "Painless PR-ing"
   homepage "https://badonde.dev"
   version "2.0.0-beta.1"
-  url "https://github.com/davdroman/Badonde/archive/#{version}.tar.gz",
+  url "https://github.com/davdroman/Badonde/archive/#{version}.tar.gz"
   head "https://github.com/davdroman/Badonde.git"
 
   depends_on :xcode => ["10", :build]
