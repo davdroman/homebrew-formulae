f415a673d2d28a432fa340fef93ed3fb3d18db1d
davdroman <d@vidroman.dev>
2019-06-18 12:39:56 +0100

    Gardening

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -1,18 +1,14 @@
 class Badonde < Formula
   desc "Painless PR-ing"
-  homepage "https://github.com/DavdRoman/Badonde"
-  url "https://github.com/DavdRoman/Badonde.git",
+  homepage "https://badonde.dev"
+  url "https://github.com/davdroman/Badonde.git",
     :tag => "2.0.0-beta.1", :revision => "0f84e1a45dfc14b8396f2758a8214fb4bfa77efc"
   version "2.0.0-beta.1"
-  head "https://github.com/DavdRoman/Badonde.git"
+  head "https://github.com/davdroman/Badonde.git"
 
-  depends_on :xcode => ["10.2", :build]
+  depends_on :xcode => ["10", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
-  end
-
-  test do
-    system "#{bin}/badonde"
   end
 end
