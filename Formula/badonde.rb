07755a5d5537fbdb39c59cf16865f6ee73645647
davdroman <d@vidroman.dev>
2019-06-18 10:55:39 +0100

    Specify `version` attribute

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -3,6 +3,7 @@ class Badonde < Formula
   homepage "https://github.com/DavdRoman/Badonde"
   url "https://github.com/DavdRoman/Badonde.git",
     :tag => "1.12.3", :revision => "213985d783d380ca05d8e1a2aec9e70c9f14ff4f"
+  version "1.12.3"
   head "https://github.com/DavdRoman/Badonde.git"
 
   depends_on :xcode => ["10.2", :build]
