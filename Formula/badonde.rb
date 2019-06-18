c35fdaf07e4be73f981f0e71e41e7e5269a69157
davdroman <d@vidroman.dev>
2019-06-18 12:46:32 +0100

    Add checksum

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -3,6 +3,7 @@ class Badonde < Formula
   homepage "https://badonde.dev"
   version "2.0.0-beta.1"
   url "https://github.com/davdroman/Badonde/archive/#{version}.tar.gz"
+  sha256 "3412a02eee8c30925211e3b4f42ada1da466d1f1eac62accb1850141d09f8afd"
   head "https://github.com/davdroman/Badonde.git"
 
   depends_on :xcode => ["10", :build]
