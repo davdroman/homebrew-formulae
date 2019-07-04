8fb6531751d7f0577bdb624045468717f56bbcc6
davdroman <d@vidroman.dev>
2019-07-04 14:08:19 +0100

    Bump version number (2.0.1.1)

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -1,9 +1,9 @@
 class Badonde < Formula
   desc "Painless PR-ing"
   homepage "https://badonde.dev"
-  version "2.0.1"
+  version "2.0.1.1"
   url "https://github.com/davdroman/Badonde/archive/#{version}.tar.gz"
-  sha256 "fba1ece94c75627d7f3d2cfca1f511919992b9bb1a96d543a631aa481944f213"
+  sha256 "010402f1023aecf119ad33e57fb8a41497253261060bc3f4b7cfc1d5de43c4db"
   head "https://github.com/davdroman/Badonde.git"
 
   depends_on :xcode => ["10", :build]
