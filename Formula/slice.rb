b32551d7f5696363d57c5a27f27b75f772fb3928
davdroman <d@vidroman.dev>
2020-08-24 12:06:32 +0100

    Update slice shasum

--- Formula/slice.rb
+++ Formula/slice.rb
@@ -3,7 +3,7 @@ class Slice < Formula
   homepage "https://github.com/davdroman/slice"
   version "master"
   url "https://github.com/davdroman/slice/archive/master.tar.gz"
-  sha256 "44e3206e2a9d00432e89e0730656c6b8883da4ee50e6331c9c81877a2865dc4f"
+  sha256 "d3f1e6ed8830621590b9eca72f199fb6fc006c791a44d5354af68a645e1429f4"
   head "https://github.com/davdroman/slice.git"
 
   def install
