0727a0524973b01799bb57f22bc5cce32c5a33ac
davdroman <d@vidroman.dev>
2020-08-24 12:03:09 +0100

    Update Badonde shasum

--- Formula/badonde.rb
+++ Formula/badonde.rb
@@ -3,7 +3,7 @@ class Badonde < Formula
   homepage "https://badonde.dev"
   version "2.0.1.1"
   url "https://github.com/davdroman/Badonde/archive/#{version}.tar.gz"
-  sha256 "010402f1023aecf119ad33e57fb8a41497253261060bc3f4b7cfc1d5de43c4db"
+  sha256 "38d4cee3e07b11a9824d1b815264a0ede9466f5beeb7b008603213d925f90e0c"
   head "https://github.com/davdroman/Badonde.git"
 
   depends_on :xcode => ["10", :build]
