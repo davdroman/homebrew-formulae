f7440e1f793de944b2a3f18825f6ac7c59e1ffb7
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-26 12:38:59 +0000

    Update kdx from 2.11.2 to 2.12.0

--- Casks/kdx.rb
+++ Casks/kdx.rb
@@ -1,6 +1,6 @@
 cask "kdx" do
-  version "2.11.2"
-  sha256 "5d48d3af08e9002c24a752f1ceb78857cf2a811351534290c1e2e7b41cccc8a4"
+  version "2.12.0"
+  sha256 "b009fb92bf2d3e459e7fcca5324db0c8cd066777f60456b7708158f51de2c08e"
 
   url "https://kdx.app/downloads/kdx-v#{version}-darwin-x64.dmg"
   name "KDX"
